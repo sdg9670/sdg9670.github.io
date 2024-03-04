396e11a1d8fefee2ad380425952580a52242849c
SimDdong <sdg9670@naver.com>
2024-03-04 15:22:19 +0900

    chroe: jekyll version update

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 npx husky install
 
 curl https://raw.githubusercontent.com/git/git/v$(git version | cut -d ' ' -f 3)/contrib/completion/git-completion.bash > ~/.git-completion.bash
-echo -e "if [ -f ~/.git-completion.bash ]; then\n    . ~/.git-completion.bash\nfi" >> ~/.bashrc
-
-git config --global user.email "sdg9670@naver.com"
+echo "if [ -f ~/.git-completion.bash ]; then\n    . ~/.git-completion.bash\nfi" >> ~/.bashrc
 git config --global user.name "SimDdong"
+git config --global user.email "sdg9670@naver.com"
 git config --global pull.rebase false
 
 source ~/.bashrc
